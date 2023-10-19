4fc7f8e92488e92cec0fb3033c6b7644b59dbe9b
JarJarDinks <Cochranejacob22@gmail.com>
2023-10-19 12:21:33 -0700

    add styles to recipe page

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,27 @@
 from django.views.generic import DetailView, ListView
 from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+import random
+import logging
 
 class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = "recipes/recipes.html"
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        all_recipes = Recipe.objects.all()
+
+        
+        random_recipes = random.sample(list(all_recipes), 3)
+
+        
+        context["random_suggestions"] = random_recipes
+
+
+        return context
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
