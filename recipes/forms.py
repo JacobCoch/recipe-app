6c7a08301a9105476ccf871801320c652ac6732e
JarJarDinks <Cochranejacob22@gmail.com>
2023-10-23 20:06:00 -0700

    add form for add_recipe, add utils for matplotlib

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,4 +4,15 @@ from .models import Recipe
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['name', 'ingredients', 'cooking_time', 'pic']
+        fields = ['name', 'cooking_time', 'pic', 'ingredients']
+
+
+CHART_CHOICES = (
+    ('#1', 'Pie Chart'),
+    ('#2', 'Bar Chart'),
+    ('#3', 'Line Chart')
+)
+class RecipeSearchForm(forms.Form):
+    recipe_name = forms.CharField(max_length=100, required=False)
+    ingredients = forms.CharField(max_length=100, required=False)
+    chart_type = forms.ChoiceField(choices=CHART_CHOICES, required=False)
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, HomeView
+from .views import RecipeListView, RecipeDetailView, HomeView, AddRecipe
 
 
 app_name = "recipes"
@@ -8,4 +8,5 @@ urlpatterns = [
     path("home/", HomeView.as_view(), name="home"),
     path("recipe/", RecipeListView.as_view(), name="recipe"),
     path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="detail"),
+    path("recipe/add/", AddRecipe.as_view(), name="add")
 ]
