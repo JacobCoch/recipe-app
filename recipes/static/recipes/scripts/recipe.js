42b188517353b93d5f75f72b5608d828d83325b8
JarJarDinks <Cochranejacob22@gmail.com>
2023-10-25 14:45:26 -0700

    navbar is finished on all pages

--- recipes/static/recipes/scripts/recipe.js
+++ recipes/static/recipes/scripts/recipe.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const addRecipe = document.getElementById('add-recipe-btn');
+  const addRecipeUrl = addRecipe.dataset.addRecipeUrl;
   const detailsButtons = document.querySelectorAll('.details-button');
+  const dropdownButton = document.getElementById('dropdownMenuButton');
+  const dropdownMenu = document.querySelector('.dropdown-menu');
+
+  // Add click event to the custom button to show/hide the dropdown
+  dropdownButton.addEventListener('click', () => {
+    if (dropdownMenu.style.display === 'block') {
+      dropdownMenu.style.display = 'none';
+    } else {
+      dropdownMenu.style.display = 'block';
+    }
+  });
+
+  // Add click event to the document to hide the dropdown when the user clicks outside of it
+  document.addEventListener('click', (e) => {
+    if (e.target.id !== 'dropdownMenuButton') {
+      dropdownMenu.style.display = 'none';
+    }
+  });
+
+  addRecipe.addEventListener(
+    'click',
+    () => (window.location.href = addRecipeUrl)
+  );
+
   detailsButtons.forEach(function (button) {
     button.addEventListener('click', function () {
       if (button) {
