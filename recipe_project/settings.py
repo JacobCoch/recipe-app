8b04a5d793f3e132578a244fd2558dfc14ead17b
JarJarDinks <Cochranejacob22@gmail.com>
2023-11-07 16:00:18 -0700

    update allowed host

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -75,7 +75,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://shrouded-forest-61463-f52cd1412c70.herokuapp.com/']
+ALLOWED_HOSTS = ['shrouded-forest-61463.herokuapp.com/']
 
 
 # Application definition
